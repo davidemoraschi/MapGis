cf2e216adbd8a471e650218428c972dc4e5bbc1f
davidemoraschi <davidem@eurostrategy.net>
2016-08-23 15:03:30 +0200

    moved to GitHub

---
+++ MapGis_console/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MapGis_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
